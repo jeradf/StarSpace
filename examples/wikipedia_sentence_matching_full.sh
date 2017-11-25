ff8f5e004c68e0dd15f3de43e5381ccaf5a5356f
jerad fields <jeradfields@gmail.com>
2017-11-25 11:58:21 -0800

    Update wikipedia_sentence_matching_full.sh

--- examples/wikipedia_sentence_matching_full.sh
+++ examples/wikipedia_sentence_matching_full.sh
@@ -12,8 +12,8 @@ DATASET=(
   wikipedia
 )
 
-MODELDIR=/tmp/starspace/models
-DATADIR=/tmp/starspace/data
+MODELDIR=/mnt/tmp/starspace/models
+DATADIR=/mnt/tmp/starspace/data
 
 mkdir -p "${MODELDIR}"
 mkdir -p "${DATADIR}"
